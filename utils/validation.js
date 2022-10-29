a06bc981351a8f50e99f60ae490920e5af31b764
Anna Ojdana <anna.ojdana@gmail.com>
2022-10-29 13:39:13 +0200

    adding pattern

--- utils/validation.js
+++ utils/validation.js
@@ -43,9 +43,7 @@ const schemaUpdateContact = Joi.object({
   name: Joi.string()
     .min(3)
     .max(50)
-    .pattern(
-      new RegExp("^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$")
-    )
+    .pattern("^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$")
     .optional(),
   email: Joi.string()
     .email({
@@ -70,17 +68,13 @@ const schemaUpdateContact = Joi.object({
   phone: Joi.string()
     .min(6)
     .max(12)
-    .pattern(
-      new RegExp(
-        "+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}"
-      )
-    )
+    .pattern("+?d{1,4}?[-.s]?(?d{1,3}?)?[-.s]?d{1,4}[-.s]?d{1,4}[-.s]?d{1,9}")
     .optional(),
 });
 
 const validationAddContact = (object) => schemaAddContact.validate(object);
-const validationUpdateContact = (object) => schemaUpdateContact.validate(object);
-
+const validationUpdateContact = (object) =>
+  schemaUpdateContact.validate(object);
 
 module.exports = {
   validationAddContact,
